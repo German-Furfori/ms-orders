d16116092f1b1f640fe22afb1fc53d8a0efcdb59
Germán Furfori <german.furfori@paygoal.com.ar>
2025-05-11 21:59:18 +0200

    Tests added for getProducts endpoint

--- src/test/java/com/immfly/msorders/MsOrdersApplicationTests.java
+++ src/test/java/com/immfly/msorders/MsOrdersApplicationTests.java
@@ -1,6 +1,7 @@
 package com.immfly.msorders;
 
 import com.immfly.msorders.entity.Category;
+import com.immfly.msorders.entity.Product;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,11 @@ public abstract class MsOrdersApplicationTests {
 		entityManager.flush();
 	}
 
+	protected void generateProductInDatabase(String name) {
+		Product product = new Product();
+		product.setName(name);
+
+		entityManager.persist(product);
+		entityManager.flush();
+	}
 }
